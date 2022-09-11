a5dfc9662af1ef353f15659f78f2bb5a06d48c5d
Alice Lepissier <alice.lepissier@gmail.com>
2022-09-11 05:58:10 +0000

    Add makefile for replication

---
+++ replicate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Rscript Scripts/DataPreparation.R
+echo "The relevant variables have been merged and cleaned."
+
+Rscript Scripts/ExploratoryDataAnalysis.R
+echo "Exploratory data analysis has been calculated."
+
+Rscript Scripts/Analysis.R
+echo "The analysis has completed."
